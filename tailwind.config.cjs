ea1055f65180383d3bab4136a55a5b6965be3bfa
silvaire-qwq <silvaire_qwq@outlook.com>
2025-07-04 08:11:35 +0800

    Auto

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
+        sans: ["Blueaka"],
       },
     },
   },
