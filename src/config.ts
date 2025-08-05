26989d72e3974891a7e1f33239a0fb7c0358c603
silvaire-qwq <silvaire_qwq@outlook.com>
2025-08-05 16:00:52 +0800

    Auto

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const siteConfig: SiteConfig = {
   subtitle: "生如夏花之绚烂，死如秋叶之静美。",
   lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
-    hue: 275, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 285, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false, // Hide the theme color picker for visitors
   },
   banner: {
