f94a5f8f32f24fbe530274ce500d38a35f8f65aa
silvaire-qwq <silvaire_qwq@outlook.com>
2025-08-08 09:34:17 +0800

    Auto

--- src/data/moments.ts
+++ src/data/moments.ts
@@ -7,6 +7,11 @@ export const momentsData = [
   {
     date: "2025-08-06",
     time: "21:56",
-    content: "羡慕后端。"
+    content: "羡慕后端。其实我之前网站是 VitePress 的时候用 Shell 写了个赤石科技，和后端功能差不多，最后才发现好像不如直接改文件好用（"
+  },
+  {
+    date: "2025-08-07",
+    time: "09:30",
+    content: "电脑由于下雨进水了，具体情况请看最新文章。"
   }
 ]
