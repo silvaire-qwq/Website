1122c1c792a7fb335f4a6522bc29c7341643cb79
Android <Android99@outlook.com>
2025-09-06 21:54:23 +0800

    fix: comments sometimes not following system theme

--- src/components/widget/Comments.svelte
+++ src/components/widget/Comments.svelte
@@ -1,37 +1,33 @@
 <section>
-    <script src="https://giscus.app/client.js"
-        data-repo={giscusConfig.repo}
-        data-repo-id={giscusConfig.repoId}
-        data-category={giscusConfig.category}
-        data-category-id={giscusConfig.categoryId}
-        data-mapping="pathname"
-        data-strict="0"
-        data-reactions-enabled="0"
-        data-emit-metadata="0"
-        data-input-position="top"
-        data-theme={$mode === DARK_MODE ? 'noborder_gray' : 'noborder_light'}
-        data-lang="zh-CN"
-        data-loading="lazy"
-        crossorigin="anonymous"
-        async>
-    </script>
+  <script src="https://giscus.app/client.js"
+    data-repo={giscusConfig.repo}
+    data-repo-id={giscusConfig.repoId}
+    data-category={giscusConfig.category}
+    data-category-id={giscusConfig.categoryId}
+    data-mapping="pathname"
+    data-strict="0"
+    data-reactions-enabled="0"
+    data-emit-metadata="0"
+    data-input-position="top"
+    data-theme={detectGiscusTheme()}
+    data-lang="zh-CN"
+    data-loading="lazy"
+    crossorigin="anonymous"
+    async>
+  </script>
 </section>
 
 <script>
-import { giscusConfig } from '@/config';
-import { AUTO_MODE, DARK_MODE } from '@constants/constants.ts'
-import { onMount } from 'svelte'
-import { writable } from 'svelte/store';
-import { getStoredTheme } from '@utils/setting-utils.ts'
-const mode = writable(AUTO_MODE)
-onMount(() => {
-  mode.set(getStoredTheme())
-})
+import { giscusConfig } from '@/config'
+
+function detectGiscusTheme() {
+  return document.documentElement.classList.contains('dark') ? 'noborder_gray' : 'noborder_light'
+}
 
 function updateGiscusTheme() {
-  const theme = document.documentElement.classList.contains('dark') ? 'noborder_gray' : 'noborder_light'
   const iframe = document.querySelector('iframe.giscus-frame')
   if (!iframe) return
+  const theme = detectGiscusTheme()
   iframe.contentWindow.postMessage({ giscus: { setConfig: { theme } } }, 'https://giscus.app')
 }
 
@@ -54,5 +50,4 @@ window.onload = () => {
   @media screen and (max-width: 767px) {
     section { margin-bottom: 14px; }
   }
-
 </style>
